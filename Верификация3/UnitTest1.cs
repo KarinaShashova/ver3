8041d9cfba9b6b9f581946a8ced5a48bc6fc9598
KarinaShashova <Ka-ra1@yandex.ru>
2017-04-29 10:23:12 +0700

    added test

---
+++ Верификация3/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Верификация3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
